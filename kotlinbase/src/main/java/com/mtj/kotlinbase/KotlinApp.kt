7f25201971a63151cc161c5eb99eba7f8dabfdcd
MTJ <mtj5x5s@163.com>
2019-06-11 08:41:44 +0800

    kotlin练习

---
+++ kotlinbase/src/main/java/com/mtj/kotlinbase/KotlinApp.kt
@@ -0,0 +1,16 @@
+package com.mtj.kotlinbase
+
+import com.zhenqi.baselibrary.base.BaseApp
+
+/**
+ * @author  mtj
+ * @time 2019/6/10 2019 06
+ * @des
+ */
+class KotlinApp : BaseApp() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ kotlinbase/src/main/java/com/mtj/kotlinbase/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.mtj.kotlinbase
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.blankj.utilcode.util.ToastUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val text = findViewById<TextView>(R.id.tv_info)
+        //text.setText("哈哈哈")
+        tv_info.setText("哈哈哈")
+        tv_info.isSelected = true
+        if(tv_info.isSelected)
+            tv_info.setText("选中")
+        else
+            tv_info.setText("未选中")
+        login()
+        tv_info.setOnClickListener { ToastUtils.showShort("哈哈哈") }
+    }
+
+
+    private fun login() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        ToastUtils.showShort("去登陆")
+    }
+
+}
